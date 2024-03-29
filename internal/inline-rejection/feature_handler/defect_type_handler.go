99798554224671fc4707b174cea55c3a5bf2d0b1
!!Timothy <jostimian@gmail.com>
2024-03-29 23:00:14 +0800

    defect_type_handler added

---
+++ internal/inline-rejection/feature_handler/defect_type_handler.go
@@ -0,0 +1,65 @@
+package feature_handler
+
+import (
+	"encoding/json"
+	"log"
+	"mackenzie/internal/entities"
+	"mackenzie/internal/features/defect_type_features"
+	"mackenzie/utils"
+	"net/http"
+	"strconv"
+)
+
+type DefectTypeFeature interface {
+	FetchAllDefectType() *[]entities.DDefectType
+	FetchDefectTypeById(id int64) *entities.DDefectType
+}
+
+type DefectTypeRestHandler struct {
+	defectFeatureHandler *defect_type_features.DefectTypeFeatureHandler
+}
+
+func NewDefectTypeRestHandler(defectFeatureHandler *defect_type_features.DefectTypeFeatureHandler) {
+	handler := &DefectTypeRestHandler{
+		defectFeatureHandler: defectFeatureHandler,
+	}
+	http.HandleFunc("/defectType", handler.FetchAllDefectTypeHandler)
+	http.HandleFunc("/defectType/by/{id}", handler.FetchDefectTypeByIdHandler)
+}
+
+func (restHandler *DefectTypeRestHandler) FetchAllDefectTypeHandler(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet:
+		allDefectType := restHandler.defectFeatureHandler.FetchAllDefectType()
+
+		allDefectTypeBytes, err := json.Marshal(allDefectType)
+		if err != nil {
+			log.Println("Something went wrong when marshaling all defect types", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+		}
+		if _, err := writer.Write(allDefectTypeBytes); err != nil {
+			log.Println("Failed writing all defect type bytes to http writer", err)
+		}
+	}
+}
+
+func (restHandler *DefectTypeRestHandler) FetchDefectTypeByIdHandler(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet:
+		idStr := utils.GetRequestQuery(writer, request, "id")
+
+		id, err := strconv.Atoi(*idStr)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+		}
+		defectType := restHandler.defectFeatureHandler.FetchDefectTypeById(int64(id))
+
+		defectTypeBytes, err := json.Marshal(defectType)
+		if err != nil {
+			log.Println("Can't marshal defectType")
+			writer.WriteHeader(http.StatusInternalServerError)
+		}
+
+		writer.Write(defectTypeBytes)
+	}
+}
--- internal/inline-rejection/ir.go
+++ internal/inline-rejection/ir.go
@@ -3,14 +3,16 @@ package inline_rejection
 import (
 	"log"
 	"mackenzie/internal/features/defect_features"
+	"mackenzie/internal/features/defect_type_features"
 	"mackenzie/internal/features/product_features"
 	"mackenzie/internal/inline-rejection/feature_handler"
 	"net/http"
 )
 
-func IRRestInterface(defectFeature *defect_features.DefectFeatureHandler, productFeature *product_features.ProductFeatureHandler) {
+func IRRestInterface(defectFeature *defect_features.DefectFeatureHandler, productFeature *product_features.ProductFeatureHandler, defectTypeFeature *defect_type_features.DefectTypeFeatureHandler) {
 	feature_handler.NewDefectRestHandler(defectFeature)
 	feature_handler.NewProductRestHandler(productFeature)
+	feature_handler.NewDefectTypeRestHandler(defectTypeFeature)
 	log.Println("Running at :8080")
 
 	err := http.ListenAndServe(":8080", nil)
