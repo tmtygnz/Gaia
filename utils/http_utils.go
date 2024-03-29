eabf14d8fb448c469a6e27969dc62a6b3fd9d944
!!Timothy <jostimian@gmail.com>
2024-03-29 22:59:54 +0800

    better `GetRequestQuery`

--- utils/http_utils.go
+++ utils/http_utils.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func GetRequestQuery(writer *http.ResponseWriter, request *http.Request, key string) *string {
+/*
+GetRequestQuery is a helper function that abstracts the process of getting a request query form the url
+*/
+func GetRequestQuery(writer http.ResponseWriter, request *http.Request, key string) *string {
 	queryStr := request.URL.Query().Get(key)
 	if queryStr == "" {
 		log.Println("query str empty.")
-		*writer.WriteHeader(http.StatusUnprocessableEntity)
-		writer.Write([]byte(err.Error()))
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+
+		//TODO: Refactor this to actually say what's missing
+		writer.Write([]byte("query missing"))
+
+		return nil
 	}
 
 	return &queryStr
