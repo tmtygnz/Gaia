93d2914a67b785cce2ed6035e3eae5bef5efad60
!!Timothy <jostimian@gmail.com>
2024-04-07 15:16:36 +0800

    feature: added area query handler

--- internal/inline-rejection/ir.go
+++ internal/inline-rejection/ir.go
@@ -1,6 +1,7 @@
 package inline_rejection
 
 import (
+	"gaia/internal/features/area_features"
 	"gaia/internal/features/defect_features"
 	"gaia/internal/features/defect_type_features"
 	"gaia/internal/features/packaging_features"
@@ -13,11 +14,15 @@ import (
 func IRRestInterface(defectFeature *defect_features.DefectFeatureHandler,
 	productFeature *product_features.ProductFeatureHandler,
 	defectTypeFeature *defect_type_features.DefectTypeFeatureHandler,
-	packagingQueryFeature *packaging_features.PackagingQueryHandler) {
+	packagingQueryFeature *packaging_features.PackagingQueryHandler,
+	areaQueryFeature *area_features.AreaQueryHandler) {
+
 	feature_handler.NewDefectRestHandler(defectFeature)
 	feature_handler.NewProductRestHandler(productFeature)
 	feature_handler.NewDefectTypeRestHandler(defectTypeFeature)
 	feature_handler.NewPackagingRestHandler(packagingQueryFeature)
+	feature_handler.NewAreaRestHandler(areaQueryFeature)
+
 	log.Println("Running at :8080")
 
 	err := http.ListenAndServe(":8080", nil)
