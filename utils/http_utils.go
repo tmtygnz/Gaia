a6dbe53f011c9e5cc1c5a4b23617aa04e4f28a35
Timothy Ganoza <jostimian@gmail.com>
2024-04-07 10:46:16 +0800

    fix: Show what parameter is missing in GetRequestQuery

--- utils/http_utils.go
+++ utils/http_utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,12 +12,9 @@ GetRequestQuery is a helper function that abstracts the process of getting a req
 func GetRequestQuery(writer http.ResponseWriter, request *http.Request, key string) *string {
 	queryStr := request.URL.Query().Get(key)
 	if queryStr == "" {
-		log.Println("query str empty.")
-		writer.WriteHeader(http.StatusUnprocessableEntity)
-
-		//TODO: Refactor this to actually say what's missing
-		writer.Write([]byte("query missing"))
-
+		errorStr := fmt.Sprintf("Parameter %s doesn't exist", key)
+		log.Println(errorStr)
+		http.Error(writer, errorStr, http.StatusBadRequest)
 		return nil
 	}
 
