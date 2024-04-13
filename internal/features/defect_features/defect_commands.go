68dacf589bc266f28207231c8059cc36be6e0750
!!Timothy <jostimian@gmail.com>
2024-04-13 15:00:24 +0800

    feature: added insert defect command

---
+++ internal/features/defect_features/defect_commands.go
@@ -0,0 +1,26 @@
+package defect_features
+
+import (
+	"gaia/internal/jet/postgres/public/model"
+	"gaia/internal/jet/postgres/public/table"
+	"gaia/provider"
+	"log"
+)
+
+type DefectCommandFeatureHandler struct {
+	db provider.IDBProvider
+}
+
+func NewDefectCommandFeatureHandler(db provider.IDBProvider) *DefectCommandFeatureHandler {
+	return &DefectCommandFeatureHandler{db: db}
+}
+
+func (commandHandler *DefectCommandFeatureHandler) InsertDefect(defect *model.Defects) error {
+	insertStmt := table.Defects.INSERT(table.Defects.MutableColumns).MODEL(defect)
+	err := commandHandler.db.Exec(insertStmt)
+	if err != nil {
+		log.Println("Unable to insert defects", err)
+		return err
+	}
+	return nil
+}
